b36e73758d5bdbe806e83f38a09b4832c3b5f471
Ethan Lu <fang.lu@gmail.com>
2023-03-25 00:38:00 -0400

    updated python solution base class to allow input to be read without stripping

--- adventofcode/common/__init__.py
+++ adventofcode/common/__init__.py
@@ -8,15 +8,21 @@ class Solution(object):
         self._year = year
         self._day = day
 
-    def _load_input_as_string(self) -> str:
+    def _load_input_as_string(self, strip: bool = True) -> str:
         file_path = f"{os.environ['ADVENT_OF_CODE_INPUT']}/{self._year}/day{self._day}.txt"
         with open(file_path) as f:
-            return f.read().replace('\n', '').strip()
+            if strip:
+                return f.read().replace('\n', '').strip()
+            else:
+                return f.read().replace('\n', '')
 
-    def _load_input_as_lines(self) -> List[str]:
+    def _load_input_as_lines(self, strip: bool = True) -> List[str]:
         file_path = f"{os.environ['ADVENT_OF_CODE_INPUT']}/{self._year}/day{self._day}.txt"
         with open(file_path) as f:
-            return [line.strip() for line in f.readlines()]
+            if strip:
+                return [line.strip() for line in f.readlines()]
+            else:
+                return [line for line in f.readlines()]
 
     def _init(self):
         raise Exception("_init not implemented!")
