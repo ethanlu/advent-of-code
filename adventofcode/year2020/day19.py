b1bdcf5bfa44a3bdd77bc5528f24e871ecfbb75d
Ethan Lu <fang.lu@gmail.com>
2023-05-09 04:05:10 -0400

    day 19 2020 part 2

--- adventofcode/year2020/day19.py
+++ adventofcode/year2020/day19.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 from adventofcode.common import Solution
-from functools import cache
+from functools import cache, lru_cache
 from itertools import product
-from typing import Dict, List, Set, Union
+from typing import Dict, List, Union
 
 
 class RegularExpressionRule(object):
@@ -26,10 +26,14 @@ class RegularExpressionRule(object):
 class RegularExpression(object):
     def __init__(self, rules: List[str]):
         self._rules: Dict[int, RegularExpressionRule] = {}
+        self._rules_length: Dict[int, int] = {}
 
         for r in rules:
             tmp = r.split(": ")
             self._rules[int(tmp[0])] = RegularExpressionRule(tmp[1])
+
+        for i in self._rules.keys():
+            self._rules_length[i] = len(self._fetch(i)[0])
 
     @cache
     def _fetch(self, r: int) -> List[str]:
@@ -49,11 +53,81 @@ class RegularExpression(object):
                 raise Exception(f"Invalid rule type : {self._rules[r].rules}")
 
     def exact_match(self, s: str) -> bool:
-        for rule in self._fetch(0):
-            if s == rule:
+        if len(s) != self._rules_length[0]:     # optimization to fail immediately if the length of s does not match the length of the rule #0 before resorting to string matching all possibilities
+            return False
+
+        for pattern in self._fetch(0):
+            if s == pattern:
                 return True
         else:
             return False
+
+
+class RegularExpressionWithLoop(RegularExpression):
+    def __init__(self, rules: List[str]):
+        super().__init__(rules)
+
+    def exact_match(self, s: str) -> bool:
+        # - only rule 0 uses rule 8 and 11
+        # - the modifications to rule 8 and 11 allow rule 42 to repeat in the beginning and rule 31 to repeat at the end
+        # - the regex pattern is basically ({rule-42})+({rule-42}){n,}({rule-31}){n,}
+        # - the number of times rule 42 matches must be greater than number of times rule 31 matches
+        # - only need to run this modified match against inputs that failed the non-looping rule
+        if super().exact_match(s):
+            return True
+        else:
+            rule42 = 0
+            rule31 = 0
+            # must match rule 42 at least twice
+            for _ in range(2):
+                if len(s) < self._rules_length[42]:
+                    return False
+                for pattern in self._fetch(42):
+                    if s[:self._rules_length[42]] == pattern:
+                        break
+                else:
+                    return False
+                s = s[self._rules_length[42]:]
+                rule42 += 1
+
+            # then can match rule 42 any number of times
+            while True:
+                if len(s) < self._rules_length[42]:
+                    break
+                for pattern in self._fetch(42):
+                    if s[:self._rules_length[42]] == pattern:
+                        break
+                else:
+                    break
+                s = s[self._rules_length[42]:]
+                rule42 += 1
+
+            # then must match rule 31 once
+            for _ in range(1):
+                if len(s) < self._rules_length[31]:
+                    return False
+                for pattern in self._fetch(31):
+                    if s[:self._rules_length[31]] == pattern:
+                        break
+                else:
+                    return False
+                s = s[self._rules_length[31]:]
+                rule31 += 1
+
+            # finally can match rule 31 any number of times
+            while True:
+                if len(s) < self._rules_length[31]:
+                    break
+                for pattern in self._fetch(31):
+                    if s[:self._rules_length[31]] == pattern:
+                        break
+                else:
+                    break
+                s = s[self._rules_length[42]:]
+                rule31 += 1
+
+            # string is exact match if remaining is 0
+            return len(s) == 0 and rule42 > rule31
 
 
 class Day19(Solution):
@@ -73,13 +147,9 @@ class Day19(Solution):
                 rules = False
 
     def part_one(self):
-        r = RegularExpression(self._rules)
-        return sum(1 for t in self._tests if r.exact_match(t))
+        re = RegularExpression(self._rules)
+        return sum(1 for t in self._tests if re.exact_match(t))
 
     def part_two(self):
-        return ""
-        self._rules[8] = "42 | 42 8"
-        self._rules[11] = "42 31 | 42 11 31"
-
-        r = RegularExpression(self._rules)
-        return sum(1 for t in self._tests if r.exact_match(t))
+        re = RegularExpressionWithLoop(self._rules)
+        return sum(1 for t in self._tests if re.exact_match(t))
