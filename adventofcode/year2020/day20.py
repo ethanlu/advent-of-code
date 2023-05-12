5e2808f4dd2555d30c82beed042d984e1f6229b9
Ethan Lu <fang.lu@gmail.com>
2023-05-12 04:02:05 -0400

    cleaned up day 20 2020 part 1 and output tiles

--- adventofcode/year2020/day20.py
+++ adventofcode/year2020/day20.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 from adventofcode.common import Solution
-from typing import Dict, List, Set
+from collections import deque
+from typing import Dict, List, Optional, Set, Union
+
+import math
 
 
 class ImageTile(object):
@@ -12,6 +15,10 @@ class ImageTile(object):
     @property
     def id(self) -> int:
         return self._id
+
+    @property
+    def size(self) -> int:
+        return self._size
 
     @property
     def top(self) -> str:
@@ -29,25 +36,18 @@ class ImageTile(object):
     def right(self) -> str:
         return "".join((self._data[y][self._size - 1] for y in range(self._size)))
 
-    def flip_x(self) -> None:
-        self._data = [list(reversed(row)) for row in self._data]
+    def flip(self) -> List[str]:
+        return ["".join(reversed(row)) for row in self._data]
 
-    def flip_y(self) -> None:
-        self._data = list(reversed(self._data))
-
-    def rotate_right(self) -> None:
+    def rotate(self) -> List[str]:
         data = [["?" for x in range(self._size)] for y in range(self._size)]
         for y in range(self._size):
             for x in range(self._size):
                 data[y][x] = self._data[self._size - 1 - x][y]
-        self._data = data
+        return ["".join(row) for row in data]
 
-    def rotate_left(self) -> None:
-        data = [["?" for x in range(self._size)] for y in range(self._size)]
-        for y in range(self._size):
-            for x in range(self._size):
-                data[y][x] = self._data[x][self._size - 1 - y]
-        self._data = data
+    def row(self, row: int) -> List[str]:
+        return self._data[row]
 
     def show(self) -> None:
         for row in self._data:
@@ -56,59 +56,216 @@ class ImageTile(object):
 
 class ImageArray(object):
     def __init__(self, tiles: Dict[int, ImageTile]):
-        self._tiles = tiles
-        self._size = len(tiles)
-        self._corners: List[ImageTile] = []
-        self._side_orientation_signatures: Dict[id, Dict[str, Set[str]]] = {}
+        self._size = int(math.sqrt(len(tiles)))
+        self._tile_size = list(tiles.values())[0].size
+        self._image: List[List[Optional[ImageTile]]] = [[None for x in range(self._size)] for y in range(self._size)]
 
-    @property
-    def corners(self) -> List[ImageTile]:
-        return self._corners
+        # store all possible orientations of each tile for future reference
+        self._tiles: Dict[int, List[ImageTile]] = {}
 
-    def _get_side_orientations(self, side: str, tile: ImageTile) -> Set[str]:
-        if tile.id in self._side_orientation_signatures and side in self._side_orientation_signatures[tile.id]:
-            return self._side_orientation_signatures[tile.id][side]
+        # store a lookup table of side signatures for each tile's side (and all orientations)
+        self._tile_side_signatures: Dict[id, Dict[str, Set[str]]] = {}
 
-        if tile.id not in self._side_orientation_signatures:
-            self._side_orientation_signatures[tile.id] = {}
-        if side not in self._side_orientation_signatures[tile.id]:
-            self._side_orientation_signatures[tile.id][side] = set([])
+        for id, tile in tiles.items():
+            self._tiles[id] = []
+            self._tile_side_signatures[id] = {
+                'top': set([]),
+                'right': set([]),
+                'bottom': set([]),
+                'left': set([])
+            }
 
-        # get the side from all 4 rotations
-        for _ in range(4):
-            self._side_orientation_signatures[tile.id][side].add(getattr(tile, side))
-            tile.rotate_right()
+            t = tile
+            for _ in range(4):
+                self._tiles[id].append(t)
+                for side in ('top', 'right', 'bottom', 'left'):
+                    self._tile_side_signatures[id][side].add(getattr(t, side))
+                t = ImageTile(id, t.rotate())
 
-        # get the side from all 4 rotations after flipping on x
-        tile.flip_x()
-        for _ in range(4):
-            self._side_orientation_signatures[tile.id][side].add(getattr(tile, side))
-            tile.rotate_left()
+            t = ImageTile(id, tile.flip())
+            for _ in range(4):
+                self._tiles[id].append(t)
+                for side in ('top', 'right', 'bottom', 'left'):
+                    self._tile_side_signatures[id][side].add(getattr(t, side))
+                t = ImageTile(t.id, t.rotate())
 
-        # get the side from all 4 rotations after flipping on y
-        tile.flip_x()
-        tile.flip_y()
-        for _ in range(4):
-            self._side_orientation_signatures[tile.id][side].add(getattr(tile, side))
-            tile.rotate_right()
+    def corners(self) -> Dict[str, Union[ImageTile, None]]:
+        return {
+            'upper left': self._image[0][0],
+            'upper right': self._image[0][self._size - 1],
+            'lower left': self._image[self._size - 1][0],
+            'lower right': self._image[self._size - 1][self._size - 1]
+        }
 
-        return self._side_orientation_signatures[tile.id][side]
+    def _can_fit(self, x: int, y: int, tile_id: int) -> Optional[ImageTile]:
+        # get all neighbors that are already placed in the image
+        neighbors = []
+        if 0 <= (y - 1) < self._size and self._image[y - 1][x] is not None:     # top neighbor
+            neighbors.append(('top', 'bottom', (self._image[y - 1][x])))
+        if 0 <= (y + 1) < self._size and self._image[y + 1][x] is not None:     # bottom neighbor
+            neighbors.append(('bottom', 'top', self._image[y + 1][x]))
+        if 0 <= (x - 1) < self._size and self._image[y][x - 1] is not None:     # left neighbor
+            neighbors.append(('left', 'right', self._image[y][x - 1]))
+        if 0 <= (x + 1) < self._size and self._image[y][x + 1] is not None:     # right neighbor
+            neighbors.append(('right', 'left', self._image[y][x + 1]))
+
+        if len(neighbors) == 0:
+            raise Exception(f"No neighbors found")
+
+        for tile in self._tiles[tile_id]:
+            for tile_side, neighbor_side, neighbor in neighbors:
+                if getattr(tile, tile_side) != getattr(neighbor, neighbor_side):
+                    # did not fit this neighbor
+                    break
+            else:
+                # this orientation fits! return
+                return tile
+
+        # all orientations failed to fit with its neighbors
+        return None
 
     def solve(self) -> None:
-        corners = []
-        # each corner has 2 unique sides that must match and the other 2 sides should not match at all
-        candidates = set(self._tiles.keys()).difference(set((c.id for c in corners)))
-        for candidate in candidates:
-            neighbor_matches = set([])
-            for candidate_side, neighbor_side in (('top', 'bottom'), ('right', 'left'), ('bottom', 'top'), ('left', 'right')):
-                candidate_sides = self._get_side_orientations(candidate_side, self._tiles[candidate])
-                for neighbor in (neighbor for neighbor in candidates if neighbor != candidate):
-                    neighbor_sides = self._get_side_orientations(neighbor_side, self._tiles[neighbor])
-                    if len(candidate_sides.intersection(neighbor_sides)) != 0:
-                        neighbor_matches.add(neighbor)
+        remaining = self._tiles.keys()
+        iteration = 0
+        while len(remaining) > 0:
+            # in each iteration, build the outer ring of tiles out of the remaining tiles that have not been placed yet. tiles in the outer ring are either corners or edges
+            corners = []
+            corner_neighbors = {}
+            edges = []
+            next_remaining = []
 
-            if len(neighbor_matches) == 2:
-                self._corners.append(self._tiles[candidate])
+            for candidate in remaining:
+                neighbor_matches = set([])
+                for candidate_side, neighbor_side in (('top', 'bottom'), ('right', 'left'), ('bottom', 'top'), ('left', 'right')):
+                    for neighbor in (neighbor for neighbor in remaining if neighbor != candidate):
+                        if len(self._tile_side_signatures[candidate][candidate_side].intersection(self._tile_side_signatures[neighbor][neighbor_side])) != 0:
+                            neighbor_matches.add(neighbor)
+
+                match len(neighbor_matches):
+                    case 0:     # tile matched no neighbors...must be only tile left
+                        corners.append(candidate)
+                    case 2:     # tiles matching neighbors on 2 sides are corners
+                        corners.append(candidate)
+                        corner_neighbors[candidate] = list(neighbor_matches)
+                    case 3:     # tiles matching neighbors on 3 sides are edges
+                        edges.append(candidate)
+                    case 4:     # tile matched neighbors on all sides, so it must in an inner ring..add it back to reminaing for later processing
+                        next_remaining.append(candidate)
+                    case _:
+                        raise Exception(f"Unexpected neighbor count : {len(neighbor_matches)}")
+
+            # build the outer ring of this iteration by starting at the upper left and going clockwise matching all tiles
+            ring_members = deque(edges)
+            if iteration == 0:      # in the first iteration, start with the corner that fits with its neighbors in the upper left
+                starting_tile = None
+                for cid in corners:
+                    for corner in self._tiles[cid]:
+                        for neighbor1, neighbor2 in ((corner_neighbors[cid][0], corner_neighbors[cid][1]), (corner_neighbors[cid][1], corner_neighbors[cid][0])):
+                            match_right = 0
+                            for corner_neighbor in self._tiles[neighbor1]:
+                                if corner.right == corner_neighbor.left:
+                                    match_right += 1
+                            match_bottom = 0
+                            for corner_neighbor in self._tiles[neighbor2]:
+                                if corner.bottom == corner_neighbor.top:
+                                    match_bottom += 1
+                            if match_right == 1 and match_bottom == 1:
+                                starting_tile = corner
+                                break
+                        if starting_tile:
+                            break
+                    if starting_tile:
+                        break
+                else:
+                    raise Exception(f"Failed to find upper left corner tile in first iteration")
+
+                self._image[iteration][iteration] = starting_tile
+                for cid in corners:
+                    if cid != starting_tile.id:
+                        ring_members.append(cid)
+            else:                   # in all other iterations, the upper left must fit with previous outer ring in the upper left
+                for cid in corners:
+                    corner = self._can_fit(iteration, iteration, cid)
+                    if corner is not None:
+                        self._image[iteration][iteration] = corner
+                    else:
+                        ring_members.append(cid)
+
+            # fill top edge from left to right
+            y = iteration
+            for x in range(iteration + 1, self._size - iteration):
+                for _ in range(len(ring_members)):
+                    member = ring_members.popleft()
+                    tile = self._can_fit(x, y, member)
+                    if tile is not None:
+                        # found a fit! add to ring and move to next member
+                        self._image[y][x] = tile
+                        break
+                    else:
+                        # did not fit, add member back to other ring members and continue fitting
+                        ring_members.append(member)
+                else:
+                    raise Exception(f"Failed to find ring member for top edge in iteration {iteration}")
+
+            # fill right edge from top to bottom
+            x = self._size - iteration - 1
+            for y in range(iteration + 1, self._size - iteration):
+                for _ in range(len(ring_members)):
+                    member = ring_members.popleft()
+                    tile = self._can_fit(x, y, member)
+                    if tile is not None:
+                        # found a fit! 1add to ring and move to next member
+                        self._image[y][x] = tile
+                        break
+                    else:
+                        # did not fit, add member back to other ring members and continue fitting
+                        ring_members.append(member)
+                else:
+                    raise Exception(f"Failed to find ring member for right edge in iteration {iteration}")
+
+            # fill bottom edge from right to left
+            y = self._size - iteration - 1
+            for x in range(self._size - iteration - 2, iteration - 1, -1):
+                for _ in range(len(ring_members)):
+                    member = ring_members.popleft()
+                    tile = self._can_fit(x, y, member)
+                    if tile is not None:
+                        # found a fit! 1add to ring and move to next member
+                        self._image[y][x] = tile
+                        break
+                    else:
+                        # did not fit, add member back to other ring members and continue fitting
+                        ring_members.append(member)
+                else:
+                    raise Exception(f"Failed to find ring member for bottom edge in iteration {iteration}")
+
+            # fill left edge from bottom to top
+            x = iteration
+            for y in range(self._size - iteration - 2, iteration, -1):
+                for _ in range(len(ring_members)):
+                    member = ring_members.popleft()
+                    tile = self._can_fit(x, y, member)
+                    if tile is not None:
+                        # found a fit! 1add to ring and move to next member
+                        self._image[y][x] = tile
+                        break
+                    else:
+                        # did not fit, add member back to other ring members and continue fitting
+                        ring_members.append(member)
+                else:
+                    raise Exception(f"Failed to find ring member for left edge in iteration {iteration}")
+
+            remaining = next_remaining
+            iteration += 1
+
+    def show(self) -> None:
+        for tile_row in self._image:
+            for data_row in range(self._tile_size):
+                line_output = []
+                for tile in tile_row:
+                    line_output.append("".join(tile.row(data_row)))
+                print(" ".join(line_output))
+            print()
 
 
 class Day20(Solution):
@@ -134,11 +291,14 @@ class Day20(Solution):
     def part_one(self):
         ia = ImageArray(self._tiles)
         ia.solve()
+        ia.show()
 
         total = 1
-        for corner in ia.corners:
-            print(f"tile {corner.id} is a corner")
-            total *= corner.id
+        for corner, tile in ia.corners().items():
+            print(f"tile {tile.id} is the {corner} corner")
+            tile.show()
+            total *= tile.id
+            print()
 
         return total
 
