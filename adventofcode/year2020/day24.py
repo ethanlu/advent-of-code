8aa05ed3140829c220ce0a05c447e0551d3c2964
Ethan Lu <fang.lu@gmail.com>
2023-05-27 22:54:08 -0400

    day 24 2020 part 2

--- adventofcode/year2020/day24.py
+++ adventofcode/year2020/day24.py
@@ -4,14 +4,16 @@ from adventofcode.common.grid import Point2D
 from typing import Dict
 
 
+directions = (Point2D(1, 1), Point2D(2, 0), Point2D(1, -1), Point2D(-1, -1), Point2D(-2, 0), Point2D(-1, 1))
+
+
 class HexGrid(object):
     def __init__(self):
         self._grid: Dict[Point2D, int] = {Point2D(0, 0): 1}
-        self._flipped = 0
 
     @property
-    def flipped(self) -> int:
-        return self._flipped
+    def black_tiles(self) -> int:
+        return sum((1 for c in self._grid.values() if c == 0))
 
     def process(self, sequence: str) -> None:
         position = Point2D(0, 0)
@@ -45,20 +47,52 @@ class HexGrid(object):
         if position not in self._grid:
             self._grid[position] = 1
         self._grid[position] = (self._grid[position] + 1) % 2
-        self._flipped += 1 if self._grid[position] == 0 else -1
+
+    def _neighbors(self, position: Point2D) -> int:
+        neighbors = 0
+        for direction in directions:
+            neighbor = position + direction
+            if neighbor in self._grid:
+                neighbors += 1 if self._grid[neighbor] == 0 else 0
+        return neighbors
+
+    def day(self) -> None:
+        black_tiles = set([])
+        white_tiles = set([])
+        for position, color in self._grid.items():
+            if color == 0:
+                black_tiles.add(position)
+                for direction in directions:
+                    neighbor = position + direction
+                    if neighbor not in self._grid or self._grid[neighbor] == 1:
+                        white_tiles.add(neighbor)
+            else:
+                white_tiles.add(position)
+        grid = {}
+        for position in black_tiles:
+            neighbors = self._neighbors(position)
+            grid[position] = 1 if neighbors == 0 or neighbors > 2 else 0
+        for position in white_tiles:
+            neighbors = self._neighbors(position)
+            grid[position] = 0 if neighbors == 2 else 1
+        self._grid = grid
 
 
 class Day24(Solution):
     def __init__(self, year: str, day: str):
         super().__init__(year, day)
         self._input = self._load_input_as_lines()
+        self._grid = HexGrid()
 
     def part_one(self):
-        hg = HexGrid()
         for s in self._input:
-            hg.process(s)
+            self._grid.process(s)
 
-        return hg.flipped
+        return self._grid.black_tiles
 
     def part_two(self):
-        return "ᕕ( ᐛ )ᕗ"
+        for _ in range(100):
+            self._grid.day()
+            print(f"day {_ + 1} : {self._grid.black_tiles}")
+
+        return self._grid.black_tiles
