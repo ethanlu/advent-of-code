297aa3ce469c74af7d7407ba86ab56bed5295e1d
Ethan Lu <fang.lu@gmail.com>
2023-12-10 23:07:58 -0500

    added utility method to print grids that are represented as dictionary of points

--- adventofcode/common/util/__init__.py
+++ adventofcode/common/util/__init__.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
-from typing import List
+from typing import Dict, List, Tuple
 
 
 def show_grid(grid: List[List[str]]) -> None:
     for row in grid:
         for col in row:
             print(f"{col}", end="")
+        print("")
+
+def show_dict_grid(grid: Dict[Tuple[int, int], str], max_x: int, max_y: int):
+    for y in range(max_y):
+        for x in range(max_x):
+            print(grid[(x, y)], end="")
         print("")
